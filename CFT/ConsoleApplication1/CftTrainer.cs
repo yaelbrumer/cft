fc2f75a850fdc9a9cb316017ec46ca82650dc8d4
eyapeleg <eyal.peleg@intel.com>
2016-02-04 22:39:19 +0200

    create cs project

---
+++ CFT/ConsoleApplication1/CftTrainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CftTrainer : IClassifier
+    {
+        ICostCalculator costCalculator;
+        IPredictor predictor;
+        ITrainer trainer;
+
+        public CftTrainer(ICostCalculator costCalculator, IPredictor predictor, ITrainer trainer)
+        {
+            this.costCalculator = costCalculator;
+            this.predictor = predictor;
+            this.trainer = trainer;
+        }
+
+        private DatasetPrediction createInitialPredictionDataSet(Dataset dataset)
+        {
+            throw new NotImplementedException();
+            return new DatasetPrediction();
+        }
+
+        private DatasetLayerK dataPreparation(DatasetPrediction dp)
+        {
+            throw new NotImplementedException();
+            return new DatasetLayerK();
+        }
+
+        private TreeClassifier createLayersClassifiers(DatasetPrediction dataset)
+        {
+            throw new NotImplementedException();
+            return new TreeClassifier();
+        }
+
+    }
+        
+    
+}
